ff5aa180f8dfdf661e80f111c0b0636fd879329c
Abhaykrb <keshavbhusal001@gmail.com>
2020-12-19 11:47:55 +0545

    revised

--- djangoInstall/settings.py
+++ djangoInstall/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'aycto@9h^l&6re&)+wdfepphs(@%dj
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['keshavrajbhusal.herokuapp.com']
+ALLOWED_HOSTS = ['keshavrajbhusal.herokuapp.com','127.0.0.1']
 
 # Application definition
 
