d1c47b53524451ce2a986f77e25602bf19729cf7
Abhaykrb <keshavbhusal001@gmail.com>
2021-02-06 08:57:19 +0545

    final comit

--- djangoInstall/settings.py
+++ djangoInstall/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
-import django_heroku
+#import django_heroku
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -134,4 +134,8 @@ MEDIA_URL='/media/'
 MEDIA_ROOT=os.path.join(BASE_DIR,"media")
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-django_heroku.settings(locals())
+#django_heroku.settings(locals())
+
+if 'DATABASE_URL' in os.environ: #means heroku
+    import dj_database_url
+    DATABASES = {'default': dj_database_url.config()} 
